2e64e1d7da3ab30a496df8e74b8ae643798e5067
Kuba <jakub.anchimowicz@gmail.com>
2023-05-14 11:21:40 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#040C18",
+        secondary: "#81AFDD",
+        white: "#ffffff",
+        orange: '#FF4820'
+      },
+      fontFamily: {
+        poppins: ["Manrope", "normal"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
