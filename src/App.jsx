4b29a7d4d387388e8a9a13988fd7e1891d437dde
Kuba <jakub.anchimowicz@gmail.com>
2023-06-12 22:54:07 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import styles from './styles'
 import { About, Blog, Features, Footer, Hero, Navbar, Register } from './components'
 
 const App = () => (
-  <div className='bg-blue-gradient w-full overflow-hidden m-[0]'>
+  <div className='bg-primary w-full overflow-hidden m-[0]'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
+    </div>
+
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
     </div>
   </div>
 )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import styles from '../styles'
+import Button from './Button'
+import { ai } from '../assets/index'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () => (
+    <section id="" className={`flex md:flex-row flex-col ${styles.paddingY} `}>
+      <div className='flex flex-row jusify-between'>
+        <div className='flex flex-col'>
+          <h1 className='text-[62px] leading-[75px] bg-lightPurple-gradient'>
+            Let’s Build Something
+            amazing with GPT-3
+            OpenAI
+          </h1>
+          <p className='text-secondary py-[33px]'>
+            Yet bed any for travelling assistance indulgence unpleasing. 
+            Not thoughts all exercise blessing. Indulgence way everything 
+            joy alteration boisterous the attachment. Party we years to 
+            order allow asked of.
+          </p>
+          <div className='flex'>
+            <input
+              placeholder='Your Email Address'
+              className='w-[200px] h-[50px] bg-input'
+            />
+            <Button content='Get Started'/>
+          </div>
+        </div>
+        <div>
+          <img src={ai} alt='ai' className='w-[1280px] h=[700px]'/>
+        </div>
+      </div>
+      <div></div>
+    </section>
+)
 
 export default Hero
